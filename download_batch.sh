e82d9fea291699a8565e6fbc3ebf08a00aaef41e
PROger4ever <PROger4ever@gmail.com>
2018-04-04 03:03:13 +0500

    implement the first version: cloning and updating only, repos from git only

---
+++ download_batch.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+DST_PATH="$1";
+
+if [ -z "$DST_PATH" ]; then
+  (echo >&2  "The first parameter not specified - a path, where the repositories should be saved.")
+  exit 1
+fi
+
+
+function handleFail() {
+  local EXIT_STATUS=$1;
+  local MSG="$2";
+  
+  if [ $EXIT_STATUS -eq 0 ]; then
+    return 0;
+  fi;
+
+  # printing? cleaning?
+  (echo >&2 "=== $MSG");
+  
+  return 3;
+}
+
+function exitOnFail() {
+  local EXIT_STATUS=$1;  
+  local MSG="$2";
+
+  handleFail $EXIT_STATUS "$MSG";
+
+  local EXIT_STATUS=$?;
+  if [ $EXIT_STATUS -ne 0 ]; then
+    exit $EXIT_STATUS
+  fi;
+}
+
+
+while IFS= read -r SRC_REPO_URL; do
+ (echo >&2 "=== Processing repo: $SRC_REPO_URL");
+  
+  REPO_NAME='';
+  if [[ "$SRC_REPO_URL" =~ /(.+)\.git ]]; then
+    REPO_NAME="${BASH_REMATCH[1]}";
+  else
+    (echo >&2 "=== Can't find repository name in the URL: $SRC_REPO_URL");
+    exit 2;
+  fi
+  
+  DST_PATH_CUR="$DST_PATH/$REPO_NAME";
+  
+  if [ -d "$DST_PATH_CUR" ]; then
+    (echo >&2 "=== updating...");
+    git -C "$DST_PATH_CUR" pull;
+  else
+    (echo >&2 "=== cloning...");
+    git clone "$SRC_REPO_URL" "$DST_PATH_CUR";
+  fi
+  
+  # submodules and other dependencies can be downloaded here
+
+  exitOnFail $? "=== Something went wrong with \"$SRC_REPO_URL\". Interrupted.";
+  echo "";
+done
+echo "done.";
